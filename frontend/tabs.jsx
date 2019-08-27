48535ae02a795819c4c036de0bb1bcf4cd9c4aaa
Michelle Kim <x1michelleskim@gmail.com>
2019-08-27 10:05:54 -0700

    Add a tabs component and add a tabsArr and render in the Root component

---
+++ frontend/tabs.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Tabs extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            
+        )
+    }
+}
+
+export default Tabs;
--- root.jsx
+++ root.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import Clock from './frontend/clock';
+import Tabs from './frontend/tabs';
+
+let tabsArr = [
+    { title: 'First', content: "Hello, I'm  first" },
+    { title: 'Second', content: "Pulot" },
+    { title: 'Third', content: 'peach' }
+]
 
 const Root = () => (
     <div>
         <h1>React is working!</h1>
         <Clock />
+        <Tabs />
     </div>
 );
 
