2691a312ba26ca6ca219ccf0addc492939034876
Michelle Kim <x1michelleskim@gmail.com>
2019-08-27 16:54:30 -0700

    Refactor render in the AutoComplete component

--- frontend/autocomplete.jsx
+++ frontend/autocomplete.jsx
@@ -8,12 +8,24 @@ class AutoComplete extends React.Component {
         this.state = {
             inputVal: ''
         }
+
+        this.handleChange = this.handleChange.bind(this);
     }
 
+    handleChange(e) {
+        e.preventDefault();
+        this.setState({ inputVal: e.target.value });
+    }
     render() {
         return (
             <div>
                 <div>This is the autocomplete</div>
+                <input type="text" value={this.state.inputVal} onChange={this.handleChange} />
+                <ul>
+                    <li>
+                        
+                    </li>
+                </ul>
             </div>
         )
     }
